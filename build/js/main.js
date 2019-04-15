04a0ee52f37ee0d5d61e88cd27ad0d280bfb1302
Sheggi9 <sheggi9@mail.ru>
2019-04-15 22:52:23 +0300

    add correct js

--- build/js/main.js
+++ build/js/main.js
@@ -2,51 +2,32 @@
 
 console.log('File 1');
 
-foo1.onclick = function () {
-    this.classList.toggle('rotate');
-    var getClass = document.querySelector(".questions__answer_1_none");
-    getClass.classList.toggle('questions__answer_block');
-};
-foo2.onclick = function () {
-    this.classList.toggle('rotate');
-    var getClass = document.querySelector(".questions__answer_2_none");
-    getClass.classList.toggle('questions__answer_block');
-};
-foo3.onclick = function () {
-    this.classList.toggle('rotate');
-    var getClass = document.querySelector(".questions__answer_3_none");
-    getClass.classList.toggle('questions__answer_block');
-};
-foo4.onclick = function () {
-    this.classList.toggle('rotate');
-    var getClass = document.querySelector(".questions__answer_4_none");
-    getClass.classList.toggle('questions__answer_block');
-};
-foo5.onclick = function () {
-    this.classList.toggle('rotate');
-    var getClass = document.querySelector(".questions__answer_5_none");
-    getClass.classList.toggle('questions__answer_block');
-};
-foo6.onclick = function () {
-    this.classList.toggle('rotate');
-    var getClass = document.querySelector(".questions__answer_6_none");
-    getClass.classList.toggle('questions__answer_block');
-};
+var questions = document.getElementsByClassName("questions__img");
+console.log(questions);
+questions = [].slice.apply(questions);
+console.log(questions);
 
-var button = document.getElementsByClassName("header__nav-menu");
-button[0].addEventListener("click", display);
+questions.forEach(function (question) {
+    question.addEventListener('click', function () {
+        this.classList.toggle('rotate');
+        this.parentNode.parentNode.classList.toggle("questions__answer_on");
+    });
+});
+
+var headerNavMenu = document.getElementsByClassName("header__nav-menu");
+headerNavMenu[0].addEventListener("click", display);
 
 function display() {
-    var bu = document.querySelector(".nav-menu_none");
-    bu.classList.toggle('nav-menu_block');
+    var classToggle = document.querySelector(".nav-menu_none");
+    classToggle.classList.toggle('nav-menu_block');
 }
 
-var button = document.getElementsByClassName("header__nav-menu-icon-img_search");
-button[0].addEventListener("click", display2);
+var iconImgSearch = document.getElementsByClassName("header__nav-menu-icon-img_search");
+iconImgSearch[0].addEventListener("click", display2);
 
 function display2() {
-    var getClass = document.querySelector(".header-search-field_none");
-    getClass.classList.toggle('header-search-field_block');
+    var classToggle = document.querySelector(".header-search-field_none");
+    classToggle.classList.toggle('header-search-field_block');
 }
 
 console.log('File 2');
--- src/js/file1.js
+++ src/js/file1.js
@@ -1,51 +1,33 @@
 console.log('File 1')
 
-foo1.onclick = function(){
-    this.classList.toggle('rotate')
-    var getClass = document.querySelector(".questions__answer_1_none");
-    getClass.classList.toggle('questions__answer_block');
-}
-foo2.onclick = function(){
-    this.classList.toggle('rotate')
-    var getClass = document.querySelector(".questions__answer_2_none");
-    getClass.classList.toggle('questions__answer_block');
-}
-foo3.onclick = function(){
-    this.classList.toggle('rotate')
-    var getClass = document.querySelector(".questions__answer_3_none");
-    getClass.classList.toggle('questions__answer_block');
-}
-foo4.onclick = function(){
-    this.classList.toggle('rotate')
-    var getClass = document.querySelector(".questions__answer_4_none");
-    getClass.classList.toggle('questions__answer_block');
-}
-foo5.onclick = function(){
-    this.classList.toggle('rotate')
-    var getClass = document.querySelector(".questions__answer_5_none");
-    getClass.classList.toggle('questions__answer_block');
-}
-foo6.onclick = function(){
-    this.classList.toggle('rotate')
-    var getClass = document.querySelector(".questions__answer_6_none");
-    getClass.classList.toggle('questions__answer_block');
-}
+let questions = document.getElementsByClassName("questions__img");
+console.log(questions);
+questions = [].slice.apply(questions);
+console.log(questions);
+
+questions.forEach(function (question){
+    question.addEventListener('click', function () {
+        this.classList.toggle('rotate')
+        this.parentNode.parentNode.classList.toggle("questions__answer_on");
+    })
+})
 
 
-var button = document.getElementsByClassName("header__nav-menu");
-button[0].addEventListener("click", display);
+
+let headerNavMenu = document.getElementsByClassName("header__nav-menu");
+headerNavMenu[0].addEventListener("click", display);
 
 function display() {
-    var bu = document.querySelector(".nav-menu_none");
-    bu.classList.toggle('nav-menu_block');
+    let classToggle = document.querySelector(".nav-menu_none");
+    classToggle.classList.toggle('nav-menu_block');
 }
 
 
-var button = document.getElementsByClassName("header__nav-menu-icon-img_search");
-button[0].addEventListener("click", display2);
+let iconImgSearch = document.getElementsByClassName("header__nav-menu-icon-img_search");
+iconImgSearch[0].addEventListener("click", display2);
 
 function display2() {
-    var getClass = document.querySelector(".header-search-field_none");
-    getClass.classList.toggle('header-search-field_block');
+    let classToggle = document.querySelector(".header-search-field_none");
+    classToggle.classList.toggle('header-search-field_block');
 }
 
